8912110bf8812ac37ca8e4c30cb6cc275cdf1506
Waislan Sanches <waislanluis@gmail.com>
2022-10-18 15:35:19 -0300

    imported geopandas

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 import os
 from osgeo import gdal, ogr, osr
+import geopandas as gpd
 
 os.chdir(r'../data/')
 
@@ -21,6 +22,9 @@ shp_layer.CreateField(new_field)
 gdal.Polygonize(band, None, shp_layer, 0, [], callback = None)
 create_shp.Destroy()
 raster = None
+
+lulc = gpd.read_file('ba_20221016_Pantanal_PUBLICx.shp')
+#lulc.head()
 
 shp_file = gpd.read_file('ba_20221016_Pantanal_PUBLICx.shp')
 shp_file.to_file('ba_20221016_Pantanal_PUBLICx.geojson', driver='GeoJSON')
